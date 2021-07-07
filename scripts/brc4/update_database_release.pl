7c461dbf6b8fb85cca1fcfbc799ae5333bea358e
Matthieu Barba <mbarba@ebi.ac.uk>
2021-07-07 15:50:00 +0100

    Use logger for messages; safer prefix removal

--- scripts/brc4/update_database_release.pl
+++ scripts/brc4/update_database_release.pl
@@ -23,13 +23,14 @@ our %opt = %{ opt_check() };
 
 my $update = $opt{update};
 my $server = $opt{server};
+my $remove_prefix = $opt{remove_prefix};
 
 my $release = software_version();
 die("Can't find the release of the current Perl API!") if not $release;
 my $ens_dir = $ENV{ENSEMBL_ROOT_DIR};
 die("Can't find the ENSEMBL_ROOT_DIR of the current Perl API!") if not $ens_dir;
-say STDERR "Current release version is $release";
-say STDERR "Ensembl root dir is $ens_dir";
+$logger->info("Current release version is $release");
+$logger->info("Ensembl root dir is $ens_dir");
 
 my $core_sql_dir = catdir($ens_dir, 'ensembl', 'sql');
 my $var_sql_dir  = catdir($ens_dir, 'ensembl-variation', 'sql');
@@ -41,15 +42,17 @@ my @connect_list = ("DBI:mysql:", "host=$opt{host}", "port=$opt{port}");
 my $connect_string = join(";", @connect_list);
 my $dbh = DBI->connect($connect_string, $opt{user}, $opt{pass}) or die("Can't connect to the server");
 
+# Get the lists of databases: core and variation
 my $databases = get_databases($dbh);
-say scalar(@$databases) . " databases in $opt{host}";
 my @core_dbs = grep { $_ =~ /_core_\d+_\d+_\d+/ } @$databases;
 my @var_dbs  = grep { $_ =~ /_variation_\d+_\d+_\d+/ } @$databases;
-say scalar(@core_dbs) . " core databases";
-say scalar(@var_dbs) . " variation databases";
+$logger->info(scalar(@$databases) . " databases in $opt{host}");
+$logger->info(scalar(@core_dbs) . " core databases");
+$logger->info(scalar(@var_dbs) . " variation databases");
 
-update_dbs($server, \@core_dbs, $release, $core_sql_dir, $update);
-update_dbs($server, \@var_dbs,  $release, $var_sql_dir,  $update);
+# Update both cores and variation dbs
+update_dbs($server, \@core_dbs, $release, $core_sql_dir, $update, $remove_prefix);
+update_dbs($server, \@var_dbs,  $release, $var_sql_dir,  $update, $remove_prefix);
 
 ###############################################################################
 sub get_databases {
@@ -61,14 +64,14 @@ sub get_databases {
 }
 
 sub update_dbs {
-  my ($server, $dbs, $cur_release, $sql_dir, $update) = @_;
+  my ($server, $dbs, $cur_release, $sql_dir, $update, $remove_prefix) = @_;
   
   for my $db (@$dbs) {
     my $db_release;
     try {
       $db_release = get_db_release($db);
     } catch {
-      warn("Can't get release from db $db: $_");
+      $logger->warn("$db\tCan't get release from db: $_");
       next;
     };
 
@@ -76,24 +79,24 @@ sub update_dbs {
     
     # Check that release versions are ok
     if (not defined $db_release) {
-      warn("$db\tDB release from db is not defined");
+      $logger->warn("$db\tDB release from db is not defined\n");
       next;
     }
     if (not defined $db_name_release) {
-      warn("$db\tCan't get db release from db name");
+      $logger->warn("$db\tCan't get db release from db name\n");
       next;
     }
     if ($db_release != $db_name_release) {
-      warn("$db\tRelease version differs between db name and meta table: $db_release vs $db_name_release\n");
+      $logger->warn("$db\tRelease version differs between db name and meta table: $db_release vs $db_name_release\n");
       next;
     }
 
     # Check if db needs to be updated
     if ($cur_release >= $db_release) {
-      my $new_db = update_db($server, $db, $cur_release, $update);
+      my $new_db = update_db($server, $db, $cur_release, $update, $remove_prefix);
       update_db_release($server, $new_db, $db_release, $cur_release, $sql_dir, $update);
     } else {
-      say STDERR "$db\tRelease is newer than current: $db_release to $cur_release" if $opt{verbose};
+      $logger->warn("$db\tRelease in db is newer than current API: $db_release to $cur_release\n");
     }
   }
 }
@@ -114,19 +117,21 @@ sub get_db_release {
 }
 
 sub update_db {
-  my ($server, $db, $cur_release, $update) = @_;
+  my ($server, $db, $cur_release, $update, $remove_prefix) = @_;
 
   my $new_db = $db;
   $new_db =~ s/_(core|variation)_(\d+)_(\d+)_(\d+)$/_$1_$2_${cur_release}_$4/;
 
   # One time prefix removal step, to be removed
-  my $remove_prefix = 1;
   if ($remove_prefix) {
-    $new_db =~ s/^[^_]+_//;
+    my ($prefix, @rest) = split("_", $new_db);
+    if ($prefix =~ /^(amoeba|crypto|fungi|giardia|hostdb|microsp|piro|plasmo|toxo|trich|tritryp|vector)/) {
+      $new_db =~ s/^${prefix}_//;
+    }
   }
 
   if ($db eq $new_db) {
-    say "$db\tNo update to the name" if $opt{verbose};
+    $logger->info("$db\tDatabase name does not change");
   } else {
     rename_db($server, $db, $new_db, $update);
   }
@@ -137,7 +142,7 @@ sub rename_db {
   my ($server, $old_name, $new_name, $update) = @_;
   
   my $command = "rename_db $server $old_name $new_name";
-  say STDERR "Use command '$command'" if $opt{debug};
+  $logger->debug("Use command '$command'");
   
   system($command) if $update;
 }
@@ -150,7 +155,7 @@ sub update_db_release {
     my @sql_files = get_sql_files($sql_dir, $rel);
     for my $file (@sql_files) {
       my $command = "$server $new_db < $file";
-      say STDERR "Use command '$command'" if $opt{debug};
+      $logger->debug("Use command '$command'");
       
       system($command) if $update;
     }
@@ -178,18 +183,21 @@ sub usage {
   }
   $help .= <<'EOF';
     Update the version of all databases in a given server
-
+    
+    Mandatory:
     --host
     --port
     --user
     --pass            : server parameters to use
     --server <str>    : server short name from mysql-cmd
     
+    Optional:
     --update          : Actually rename databases
+    --remove_prefix   : Remove a brc4 prefix from the database name
     
     --help            : show this help message
     --verbose         : show detailed progress
-    --debug           : show even more information (for debugging purposes)
+    --debug           : show even more information
 EOF
   print STDERR "$help\n";
   exit(1);
@@ -204,6 +212,7 @@ sub opt_check {
     "pass=s",
     "server=s",
     "update",
+    "remove_prefix",
     "help",
     "verbose",
     "debug",
