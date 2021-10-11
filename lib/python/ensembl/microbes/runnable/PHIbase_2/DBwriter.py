9816c14def039847d15c7e36a646f27039c3dcb0
Manuel Carbajo <mcarbajo@codon-login-03.ebi.ac.uk>
2021-10-11 16:43:28 +0100

    writes values to ensembl_gene

--- lib/python/ensembl/microbes/runnable/PHIbase_2/DBwriter.py
+++ lib/python/ensembl/microbes/runnable/PHIbase_2/DBwriter.py
@@ -83,13 +83,18 @@ class DBwriter(eHive.BaseRunnable):
         patho_uniprot_id = self.param_required('patho_uniprot_id')
         patho_sequence = self.param_required('patho_sequence')
         patho_gene_name = self.param_required('patho_gene_name')
-        patho_specie_taxon_id = int(self.param_required('patho_specie_taxon_id'))
+        patho_species_taxon_id = int(self.param_required('patho_species_taxon_id'))
         patho_species_name = self.param_required('patho_species_name')
+        patho_core_dbname = self.param_required("patho_core_dbname")
+        patho_division = self.param_required("patho_division")
         patho_species_strain = self.param_required('patho_species_strain')
         host_uniprot_id = self.param_required('host_uniprot_id')
         host_gene_name = self.param_required('host_gene_name')
         host_species_taxon_id = int(self.param_required('host_species_taxon_id'))
         host_species_name = self.param_required('host_species_name')
+        host_core_dbname = self.param_required("host_core_dbname")
+        host_division = self.param_required("host_division")
+
         litterature_id = self.param_required('litterature_id')
         litterature_source = self.param_required('litterature_source')
         doi = self.param_required('doi')
@@ -103,32 +108,41 @@ class DBwriter(eHive.BaseRunnable):
         source_db_value = self.get_source_db_value(session, db_label)
         self.add_if_not_exists(session, source_db_value)
         
-        print(f'pathogen_species_value TO ADD -- {patho_species_name}')
-        pathogen_species_value = self.get_species_value(session, patho_specie_taxon_id)
+        print(f'pathogen_species_value TO ADD -- {patho_species_name} division {patho_division} coreDB {patho_core_dbname}')
+        pathogen_species_value = self.get_species_value(session, patho_species_taxon_id, patho_division, patho_species_name)
         self.add_if_not_exists(session, pathogen_species_value)
         print(f'pathogen_species_value AFTER ADD -- {pathogen_species_value}')
         
-        print(f'host_species_value TO ADD -- {host_species_name}')
-        host_species_value = self.get_species_value(session, host_species_taxon_id)
+        print(f'host_species_value TO ADD -- {host_species_name} division {host_division} coreDB {host_core_dbname}')
+        host_species_value = self.get_species_value(session, host_species_taxon_id, host_division, host_species_name)
         self.add_if_not_exists(session, host_species_value)
         print(f'host_species_value AFTER ADD -- {host_species_value}')
     
         print(f'pathogen_gene_name -- {patho_gene_name} -- division -- {pathogen_species_value.ensembl_division}')
-        self.get_ensembl_id(patho_gene_name, patho_specie_taxon_id, pathogen_species_value.ensembl_division)
+        patho_ensembl_stable_id = self.get_ensembl_id(patho_gene_name, patho_species_taxon_id, pathogen_species_value.ensembl_division)
+        host_ensembl_stable_id = self.get_ensembl_id(host_gene_name, host_species_taxon_id, host_species_value.ensembl_division)
+
+        patho_species_id = session.query(interaction_db_models.Species).filter_by(species_taxon_id=patho_species_taxon_id).one().species_id
+        host_species_id = session.query(interaction_db_models.Species).filter_by(species_taxon_id=host_species_taxon_id).one().species_id
+        patho_ensembl_gene_value = self.get_ensembl_gene_value(session, patho_ensembl_stable_id,patho_species_id)
+        host_ensembl_gene_value = self.get_ensembl_gene_value(session, host_ensembl_stable_id,host_species_id)
+
+        self.add_if_not_exists(session, patho_ensembl_gene_value)
+        self.add_if_not_exists(session, host_ensembl_gene_value)
 
     def get_ensembl_id(self,gene_name, tax_id ,division):
-        
+        result = None
         reported = False
         gene_names = gene_name.split(';')
         for gn in gene_names:
             if " Ensembl: " in gn:
-                print (gn.replace(' Ensembl: ',''))
+                result = gn.replace(' Ensembl: ','')
+                print ('Ensembl_id:' + result)
                 reported = True
-
+                
         if not reported:
             print ("Mmmm..., we need to find out how we name this gene name in ensembl: " + gene_name)
-
-        core_db = self.get_core_db_name(tax_id)
+        return result
 
     def add_if_not_exists(self, session, value):
         session.add(value)
@@ -144,26 +158,6 @@ class DBwriter(eHive.BaseRunnable):
             print(e)
             session.rollback()
 
-    def get_species_name(self, taxon_id):
-        species_name = None
-        sql="SELECT name FROM ncbi_taxa_name WHERE taxon_id=%d AND name_class='scientific name'"
-        
-        self.db = pymysql.connect(host=self.param('meta_host'),user=self.param('meta_user'),db='ncbi_taxonomy',port=self.param('meta_port'))
-        self.cur = self.db.cursor()
-        try:
-            self.cur.execute(sql % taxon_id)
-            self.db.commit()
-        except pymysql.Error as e:
-            try:
-                print ("Mysql Error:- "+str(e))
-            except IndexError:
-                print ("Mysql Error:- "+str(e))
-                self.connection_close()
-        for row in self.cur:
-            species_name = row[0]
-        self.db.close()
-        print("species_name:" + species_name)
-        return species_name
 
     def get_db_label(self):
         return 'PHI-base'
@@ -179,9 +173,7 @@ class DBwriter(eHive.BaseRunnable):
         
         return source_db_value
 
-    def get_species_value(self, session, species_tax_id):
-        division = self.get_division(species_tax_id)
-        species_name = self.get_species_name(species_tax_id)
+    def get_species_value(self, session, species_tax_id, division, species_name):
         
         try:
             species_value = session.query(interaction_db_models.Species).filter_by(species_taxon_id=species_tax_id).one()
@@ -190,62 +182,15 @@ class DBwriter(eHive.BaseRunnable):
         except NoResultFound:
             species_value = interaction_db_models.Species(ensembl_division=division, species_production_name=species_name,species_taxon_id=species_tax_id)
         return species_value
+    
 
-    def get_core_db_name(self, tax_id):
-        core_db_name = None
-        sql = "select gd.dbname from organism o join genome g using(organism_id) join genome_database gd using(genome_id) where o.species_taxonomy_id=%d and gd.type='core' and g.data_release_id=(select dr.data_release_id from data_release dr where is_current=1)"
-
-        self.db = pymysql.connect(host=self.param('meta_host'),user=self.param('meta_user'),db='ensembl_metadata',port=self.param('meta_port'))
-        self.cur = self.db.cursor()
+    def get_ensembl_gene_value(self, session, stable_id,species_id):
         try:
-            self.cur.execute( sql % tax_id)
-            self.db.commit()
-        except pymysql.Error as e:
-            try:
-                print ("Mysql Error:- "+str(e))
-            except IndexError:
-                print ("Mysql Error:- "+str(e))
-        self.db.close()
-        for row in self.cur:
-            core_db_name = row[0]
-
-        print (f"core_DB -- {core_db_name}")
-        return core_db_name
-
-    def get_division(self, species_tax_id):
-        division = None
-        sql="SELECT DISTINCT d.short_name FROM genome g JOIN organism o USING(organism_id) JOIN division d USING(division_id) WHERE short_name != 'EV' AND species_taxonomy_id=%d"
-        
-        self.db = pymysql.connect(host=self.param('meta_host'),user=self.param('meta_user'),db='ensembl_metadata',port=self.param('meta_port'))
-        self.cur = self.db.cursor()
-        try:
-            self.cur.execute( sql % species_tax_id)
-            self.db.commit()
-        except pymysql.Error as e:
-            try:
-                print ("Mysql Error:- "+str(e))
-            except IndexError:
-                print ("Mysql Error:- "+str(e))
-                self.connection_close()
-        
-        for row in self.cur:
-            division = row[0]
-        
-        if division == 'EF':
-            division='fungi'
-        elif division == 'EPl':
-            division='plants'
-        elif division == 'EPr':
-            division='protists'
-        elif division == 'EB':
-            division='bacteria'
-        else:
-            print ("Division not recognised:" + str(division))
-            raise ValueError("Weird... That division is not supposed to be here")
-
-        self.db.close()
-        print("division:" + str(division))
-
-        return division
+            ensembl_gene_value = session.query(interaction_db_models.EnsemblGene).filter_by(ensembl_stable_id=stable_id).one()
+        except MultipleResultsFound:
+            print(f"ERROR: Multiple results found for species: {species_id} - gene stable_id {stable_id}")
+        except NoResultFound:
+            ensembl_gene_value = interaction_db_models.EnsemblGene(ensembl_stable_id=stable_id, species_id=species_id,import_time_stamp=db.sql.functions.now())
+        return ensembl_gene_value
 
 
