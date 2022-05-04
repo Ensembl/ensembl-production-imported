eab6281201dec729d20f9aa6e2400aaa6fa1e99a
Matthieu Barba <mbarba@ebi.ac.uk>
2022-05-04 12:00:46 +0100

    Add docstring doc to script

--- scripts/brc4/get_rnaseq_from_redmine.py
+++ scripts/brc4/get_rnaseq_from_redmine.py
@@ -1,12 +1,14 @@
 #!env python3
 
-from typing import List
+from typing import Dict, List, Tuple
 import argparse
 import json
 import re
 from unidecode import unidecode
 from pathlib import Path
 from redminelib import Redmine
+from redminelib.resources import Issue as RedmineIssue
+from redminelib.managers import ResourceManager as RedmineResource
 
 url = 'https://redmine.apidb.org'
 default_fields = dict(
@@ -18,9 +20,16 @@ accession_api_url = "https://www.ebi.ac.uk/ena/browser/api/xml/%s"
 veupathdb_id = 1976
 
 
-def load_abbrevs(path) -> List[str]:
+def load_abbrevs(path: str) -> List[str]:
     """
-    Load a list of organism abbrevs from a file. Expected to be one per line
+    Load a list of organism abbrevs from a file. Expected to be one per line.
+
+    Args:
+        path: Path to the organism abbrevs file.
+
+    Returns:
+        A list of all organism_abbrevs.
+
     """
     if not path:
         print("Warning: I don't have a list of older abbrevs to compare with.")
@@ -39,10 +48,17 @@ def load_abbrevs(path) -> List[str]:
     return abbrevs
 
 
-def retrieve_rnaseq_datasets(redmine, output_dir_path, build=None, abbrevs_file=None):
+def retrieve_rnaseq_datasets(redmine: Redmine, output_dir_path: str, build: int = None,
+                             abbrevs_file: str = None) -> None:
     """
     Get RNA-Seq metadata from Redmine, store them in json files.
-    Each issue/dataset is stored as one file in the output dir
+    Each issue/dataset is stored as one file in the output dir.
+
+    Args:
+        redmine: A connected Redmine object.
+        output_dir_path: Directory where the dataset files are to be stored.
+        build: BRC build number.
+        abbrevs_file: Path to a list of organism_abbrevs that are already in use.
     """
 
     all_abbrevs = load_abbrevs(abbrevs_file)
@@ -117,7 +133,17 @@ def retrieve_rnaseq_datasets(redmine, output_dir_path, build=None, abbrevs_file=
         json.dump(all_datasets, f, indent=True)
 
        
-def print_summaries(summaries, description):
+def print_summaries(summaries: Dict, description: str) -> None:
+    """Print a summary of various counts.
+
+    This will print one line for each issue in the dict, with its description, the issue id
+    and the issue subject.
+
+    Args:
+        summaries: Dict with 2 keys:
+            issue: A Redmine Issue object.
+            desc: A description for that issue.
+    """
     desc_length = 64
     
     if summaries:
@@ -129,10 +155,21 @@ def print_summaries(summaries, description):
             print(f"\t{desc:{desc_length}}\t{issue.id}\t({issue.subject})")
     
  
-def parse_dataset(issue):
+def parse_dataset(issue: RedmineIssue) -> Tuple[Dict, str]:
     """
-    Extract RNA-Seq dataset metadata from a Redmine issue
-    Return a nested dict
+    Extract RNA-Seq dataset metadata from a Redmine issue.
+
+    Args:
+        issue: A Redmine issue.
+
+    Returns:
+        A tuple of 2 objects:
+            datasets: A dict representing a dataset, with the following keys:
+                component: String for the BRC component DB.
+                species: String for the organism abbrev.
+                name: String for the internal dataset name.
+            problem: A string description if there was a parsing problem
+            (empty string otherwise).
     """
     customs = get_custom_fields(issue)
     dataset = {
@@ -171,13 +208,28 @@ def parse_dataset(issue):
     return dataset, problem
 
 
-def check_organism_abbrev(name):
-    """Limited check: do not accept organism_abbrevs with special characters"""
+def check_organism_abbrev(name: str) -> bool:
+    """Check the organism_abbrevs string format to avoid special characters.
+
+    Args:
+        name: organism_abbrev to check.
+    
+    Returns:
+        True if the organism_abbrev format is correct.
+        False otherwise.
+    """
     return not re.search(r'[ \/\(\)#\[\]:]', name)
 
 
-def normalize_name(old_name):
-    """Remove special characters, keep ascii only"""
+def normalize_name(old_name: str) -> str:
+    """Remove special characters from an organism_abbrev, keep ascii only.
+
+    Args:
+        old_name: the organism_abbrev to format.
+    
+    Returns:
+        The formatted organism_abbrev.
+    """
     
     # Remove any diacritics
     name = old_name.strip()
@@ -195,7 +247,17 @@ def normalize_name(old_name):
     return name
 
 
-def parse_samples(sample_str):
+def parse_samples(sample_str: str) -> List[Dict]:
+    """Parse a list of samples from a Redmine task.
+
+    Args:
+        sample_str: The value of the field 'Sample Names' from an RNA-Seq Redmine task.
+    
+    Returns:
+        A list of samples dicts, with the following keys:
+            name: the name of the sample.
+            accessions: a list of string representing the SRA accessions for that sample.
+    """
     samples = []
     
     # Parse each line
@@ -242,9 +304,15 @@ def parse_samples(sample_str):
     return samples
 
 
-def validate_accessions(accessions):
-    """
-    Check the accessions, to make sure we get proper ones
+def validate_accessions(accessions: str) -> bool:
+    """Check SRA accessions format, to make sure we get proper ones.
+
+    Args:
+        accessions: a list of strings to check
+
+    Return:
+        True if all strings are proper SRA accessions.
+        False if at least one is not a proper SRA accession.
     """
     if "" in accessions:
         return False
@@ -254,10 +322,14 @@ def validate_accessions(accessions):
     return True
 
 
-def get_custom_fields(issue):
-    """
-    Put all Redmine custom fields in a dict instead of an array
-    Return a dict
+def get_custom_fields(issue: RedmineIssue) -> Dict:
+    """Put all Redmine custom fields in a dict instead of an array.
+
+    Args:
+        issue: A Redmine issue.
+    
+    Returns:
+        A dict where each key is a custom field.
     """
     
     cfs = {}
@@ -266,7 +338,18 @@ def get_custom_fields(issue):
     return cfs
 
 
-def get_custom_value(customs, key):
+def get_custom_value(customs: Dict, key: str) -> str:
+    """Retrieve a custom value from a customs dict.
+
+    Args:
+        customs: Dict of customs values gotten from get_custom_fields.
+        key: Key to extract the value from the custom dict.
+    
+    Returns:
+        A single value.
+        Throws an exception if there are more than 1 value.
+        If there is no such key in the dict, return an empty string.
+    """
    
     try:
         value = customs[key]["value"]
@@ -281,10 +364,16 @@ def get_custom_value(customs, key):
         return ""
     
 
-def get_issues(redmine, datatype, build=None):
-    """
-    Retrieve all issue for new genomes, be they with or without gene sets
-    Return a Redmine ResourceSet
+def get_issues(redmine: Redmine, datatype: str, build: int = None) -> List[RedmineIssue]:
+    """Retrieve all issue for new genomes, be they with or without gene sets.
+
+    Args:
+        redmine: A Redmine connected object.
+        datatype: What datatype to use to filter the issues.
+        build: The BRC build to use to filter.
+
+    Returns:
+        A list of Redmine issues.
     """
     
     other_fields = {"cf_94": datatype}
@@ -295,9 +384,15 @@ def get_issues(redmine, datatype, build=None):
     return list(get_ebi_issues(redmine, other_fields))
 
 
-def get_version_id(redmine, build):
-    """
-    Given a build number, get the version id for it
+def get_version_id(redmine: Redmine, build: int) -> int:
+    """Given a build number, get the Redmine version id for it.
+
+    Args:
+        redmine: A Redmine connected object.
+        build: The BRC build to use to filter.
+
+    Returns:
+        The version id from Redmine for that build.
     """
     versions = redmine.version.filter(project_id=veupathdb_id)
     version_name = "Build " + str(build)
@@ -305,10 +400,15 @@ def get_version_id(redmine, build):
     return version_id
 
    
-def get_ebi_issues(redmine, other_fields=dict()):
-    """
-    Get EBI issues from Redmine, add other fields if provided
-    Return a Redmine ResourceSet
+def get_ebi_issues(redmine, other_fields=dict()) -> RedmineResource:
+    """Get EBI issues from Redmine, add other fields if provided.
+
+    Args:
+        redmine: A Redmine connected object.
+        other_fields: A dict of fields to provide to filter the issues.
+
+    Returns:
+        A Redmine resource set.
     """
     # Other fields replace the keys that already exist in default_fields
     search_fields = {**default_fields, **other_fields}
@@ -317,7 +417,6 @@ def get_ebi_issues(redmine, other_fields=dict()):
     
 
 def main():
-    # Parse command line arguments
     parser = argparse.ArgumentParser(description='Retrieve metadata from Redmine')
     
     parser.add_argument('--key', type=str, required=True,
